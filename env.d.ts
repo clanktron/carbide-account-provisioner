078cac67ec3633e44ca45e8375d3ce2bb33d6242
clayton <hello@claytonc.dev>
2024-12-23 17:21:43 -0800

    add relevant harbor robotCreate types (auto-generated from harbor repo)

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,90 @@
 /// <reference types="vite/client" />
+
+// example RobotCreate
+//
+// {
+//   "permissions": [
+//     {
+//       "access": [
+//         {
+//           "resource": "registry",
+//           "action": "read"
+//         }
+//       ],
+//       "kind": "system",
+//       "namespace": "/"
+//     }
+//   ],
+//   "name": "another-test",
+//   "description": "testing description yahoo",
+//   "duration": 45,
+//   "disable": false,
+//   "level": "system"
+// }
+
+// the following types were auto-generated using ng-swagger-gen in the goharbor/harbor project @ commit a548ab705f8cb713ad0e4a9fb4e55c8b80237ac8
+export interface RobotCreate {
+
+  /**
+   * The name of the robot
+   */
+  name?: string;
+
+  /**
+   * The description of the robot
+   */
+  description?: string;
+
+  /**
+   * The secret of the robot
+   */
+  secret?: string;
+
+  /**
+   * The level of the robot, project or system
+   */
+  level?: string;
+
+  /**
+   * The disable status of the robot
+   */
+  disable?: boolean;
+
+  /**
+   * The duration of the robot in days, duration must be either -1(Never) or a positive integer
+   */
+  duration?: number;
+  permissions?: Array<RobotPermission>;
+}
+
+export interface RobotPermission {
+
+  /**
+   * The kind of the permission
+   */
+  kind?: string;
+
+  /**
+   * The namespace of the permission
+   */
+  namespace?: string;
+  access?: Array<Access>;
+}
+
+export interface Access {
+
+  /**
+   * The resource of the access. Possible resources are listed here for system and project level https://github.com/goharbor/harbor/blob/main/src/common/rbac/const.go
+   */
+  resource?: string;
+
+  /**
+   * The action of the access. Possible actions are *, pull, push, create, read, update, delete, list, operate, scanner-pull and stop.
+   */
+  action?: string;
+
+  /**
+   * The effect of the access
+   */
+  effect?: string;
+}
