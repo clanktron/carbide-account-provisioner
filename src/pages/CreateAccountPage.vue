c47bd1203e0c01be287220bef8b9aeac981360fb
clayton <hello@claytonc.dev>
2024-12-23 17:20:30 -0800

    setup app scaffolding/routing

---
+++ src/pages/CreateAccountPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <h3 class="card-title text-center fw-bold">Create Carbide Account</h3>
+      <form id="accountForm">
+          <div class="form-floating mb-3 m-4 text-truncate">
+              <input type="name" class="form-control" id="floatingInput" placeholder="Account Name" v-model="accountName" required>
+              <!-- <label for="floatingInput">Enter Username</label> -->
+          </div>
+          <div class="form-floating mb-3 m-4 text-truncate">
+              <input type="description" class="form-control" id="floatingInput" placeholder="Description" v-model="accountDescription" required>
+              <!-- <label for="floatingInput">Enter Password</label> -->
+          </div>
+          <div class="form-floating mb-3 m-4 text-truncate">
+              <input type="duration" class="form-control" id="floatingInput" placeholder="Duration" v-model="accountDuration" required>
+              <!-- <label for="floatingInput">Enter Password</label> -->
+          </div>
+      </form>
+      <div class="text-center">
+          <button class="btn btn-primary" type="button">Submit</button>
+      </div>
+      <div v-if="errorMessage" class="text-danger text-center mt-2">{{ errorMessage }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const accountName = ref('');
+const accountDescription = ref('');
+const accountDuration = ref('');
+const errorMessage = ref('');
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginPage from '../pages/LoginPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/login'
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/login',
+      name: 'login',
+      component: LoginPage,
+    },
+    {
+      path: '/account',
+      name: 'account',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../pages/CreateAccountPage.vue'),
     },
   ],
 })
