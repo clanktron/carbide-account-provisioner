377a00d37007418de05ae1f7854a755511e883d1
clayton <hello@claytonc.dev>
2024-12-26 17:48:03 -0800

    cleanup vue markup

--- src/pages/CreateAccountPage.vue
+++ src/pages/CreateAccountPage.vue
@@ -3,16 +3,16 @@
       <h3 class="card-title text-center fw-bold">Create Carbide Account</h3>
       <form id="accountForm">
           <div class="form-floating mb-3 m-4 text-truncate">
-              <input type="name" class="form-control" id="floatingInput" placeholder="Account Name" v-model="accountName" required>
-              <!-- <label for="floatingInput">Enter Username</label> -->
+              <input type="name" class="form-control" id="accountName" placeholder="Account Name" v-model="accountName" required>
+              <label for="accountName">Enter Account Name</label>
           </div>
           <div class="form-floating mb-3 m-4 text-truncate">
-              <input type="description" class="form-control" id="floatingInput" placeholder="Description" v-model="accountDescription" required>
-              <!-- <label for="floatingInput">Enter Password</label> -->
+              <input type="description" class="form-control" id="description" placeholder="Description" v-model="accountDescription" required>
+              <label for="description">Enter Account Description</label>
           </div>
           <div class="form-floating mb-3 m-4 text-truncate">
-              <input type="duration" class="form-control" id="floatingInput" placeholder="Duration" v-model="accountDuration" required>
-              <!-- <label for="floatingInput">Enter Password</label> -->
+              <input type="duration" class="form-control" id="duration" placeholder="Duration" v-model="accountDuration" required>
+              <label for="duration">Enter Days Till Expiry</label>
           </div>
       </form>
       <div class="text-center">
