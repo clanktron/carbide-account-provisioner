45a95bc342b28f2c8fbbe6139def1b25eb376729
clayton <hello@claytonc.dev>
2024-12-26 19:08:48 -0800

    preliminary form based submission for harbor login

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,29 +1,48 @@
 <template>
   <div>
-      <h3 class="text-center fw-bold">Sign In</h3>
-      <form id="loginForm">
-          <div class="form-floating mb-3 m-4 text-truncate">
-              <input type="username" class="form-control" id="floatingUsername" placeholder="Username" v-model="username" required>
-              <label for="floatingUsername">Enter Username</label>
-          </div>
-          <div class="form-floating mb-3 m-4 text-truncate">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password" v-model="password" required>
-              <label for="floatingPassword">Enter Password</label>
-          </div>
-      </form>
-      <div class="text-center">
-          <button class="btn btn-primary" type="button" >Login</button>
+    <h3 class="text-center fw-bold">Sign In</h3>
+    <form
+      id="loginForm"
+      method="POST"
+      action="https://harbor.claytonc.dev/c/login"
+    >
+      <div class="form-floating mb-3 m-4 text-truncate">
+        <input
+          type="text"
+          name="principal"
+          class="form-control"
+          id="floatingUsername"
+          placeholder="Username"
+          v-model="username"
+          required
+        >
+        <label for="floatingUsername">Enter Username</label>
       </div>
-      <div v-if="errorMessage" class="text-danger text-center mt-2">{{ errorMessage }}</div>
+      <div class="form-floating mb-3 m-4 text-truncate">
+        <input
+          type="password"
+          name="password"
+          class="form-control"
+          id="floatingPassword"
+          placeholder="Password"
+          v-model="password"
+          required
+        >
+        <label for="floatingPassword">Enter Password</label>
+      </div>
+      <div class="text-center">
+        <button class="btn btn-primary" type="submit">Login</button>
+      </div>
+    </form>
+    <div v-if="errorMessage" class="text-danger text-center mt-2">{{ errorMessage }}</div>
   </div>
-</template>
+</template>/template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 const username = ref('');
 const password = ref('');
 const errorMessage = ref('');
-// @submit.prevent="submitForm"
 // @click="submitForm"
 // async function submitForm() {
 //     try {
