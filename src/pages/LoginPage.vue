bc7da1dbd5b54a1b87e58d18b1d4ad3b9a0aafae
clayton <hello@claytonc.dev>
2024-12-26 17:45:14 -0800

    adjust preliminary css

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,43 +1,9 @@
 <template>
-  <div>
-      <h3 class="card-title text-center fw-bold">Sign In</h3>
-      <form id="loginForm">
-          <div class="form-floating mb-3 m-4 text-truncate">
-              <input type="username" class="form-control" id="floatingInput" placeholder="Username" v-model="username" required>
-              <!-- <label for="floatingInput">Enter Username</label> -->
-          </div>
-          <div class="form-floating mb-3 m-4 text-truncate">
-              <input type="password" class="form-control" id="floatingInput" placeholder="Password" v-model="password" required>
-              <!-- <label for="floatingInput">Enter Password</label> -->
-          </div>
-      </form>
-      <div class="text-center">
-          <button class="btn btn-primary" type="button" >Login</button>
-      </div>
-      <div v-if="errorMessage" class="text-danger text-center mt-2">{{ errorMessage }}</div>
+  <div class="min-vh-100 d-flex flex-column align-items-center justify-content-center">
+    <login/>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-const username = ref('');
-const password = ref('');
-const errorMessage = ref('');
-// @submit.prevent="submitForm"
-// @click="submitForm"
-// async function submitForm() {
-//     try {
-//         const authorized = await login(license.value)
-//         if (authorized) {
-//             console.debug("Login attempt succeeded, navigating to dashboard...")
-//             navigateTo('/dashboard')
-//         } else {
-//             console.debug("Login attempt failed")
-//             errorMessage.value = "Invalid license. Please try again.";
-//         }
-//     } catch (error) {
-//         console.error("error:", error);
-//         errorMessage.value = "An error occurred while processing your request. Please try again later.";
-//     }
-// }
+import login from '@/components/login.vue';
 </script>
