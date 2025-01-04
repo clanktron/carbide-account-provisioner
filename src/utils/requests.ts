4be97052a25a23b60460fbbca4b7e85157e93512
clayton <hello@claytonc.dev>
2025-01-03 17:26:02 -0800

    refactor API requests

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,75 +1,74 @@
 import type { Robot, RobotCreate } from "../../env"
+import router from "@/router";
 
-export async function GetRobots(): Promise<Robot[]> {
+export function getSavedCredentials(): string {
+    const authString = sessionStorage.getItem('authString')
+    if (authString == null) {
+        throw new Error("missing basic auth credentials");
+    }
+    return authString
+}
+
+function handleAuthError() {
+    sessionStorage.removeItem('authString');
+    router.push('/');
+}
+
+async function apiRequest(url: string, method: string, body?: any): Promise<any> {
     try {
+        const authString = getSavedCredentials();
+        const response = await fetch(url, {
+            method,
+            headers: { 'Authorization': 'Basic ' + authString },
+            credentials: "omit",
+            body: body ? JSON.stringify(body) : undefined,
+        });
+
+        if (response.ok) {
+            return await response.json();
+        } else if (response.status === 401) {
+            handleAuthError();
+            throw new Error("Invalid credentials");
+        } else {
+            throw new Error("Invalid response from server");
+        }
+    } catch (error) {
+        handleAuthError();
+        throw error;
+    }
+}
+
+export async function TestCredentials(username: string, password: string): Promise<boolean> {
+    try {
+        const authString = btoa(username+':'+password)
         const response = await fetch(`/api/v2.0/robots`, {
             method: "GET",
-            headers: {'Authorization': 'Basic ' + btoa(username+':'+password)},
+            headers: {'Authorization': 'Basic ' + authString},
             credentials: "omit",
         });
         if (response.ok) {
-            const robots: Robot[] = await response.json()
-            return robots
+            sessionStorage.setItem('authString', authString)
+            return true
         } else {
             throw new Error("invalid response from server");
         }
     } catch (error) {
         throw error;
     }
+}
+
+export async function GetRobots(): Promise<Robot[]> {
+    return await apiRequest(`/api/v2.0/robots`, "GET");
 }
 
 export async function CreateRobot(robot: RobotCreate): Promise<Robot> {
-    try {
-        const response = await fetch(`/api/v2.0/robots`, {
-            method: "POST",
-            headers: {'Authorization': 'Basic ' + btoa(username+':'+password)},
-            credentials: "omit",
-            body: JSON.stringify(robot)
-        });
-        if (response.ok) {
-            const robot: Robot = await response.json()
-            return robot
-        } else {
-            throw new Error("invalid response from server");
-        }
-    } catch (error) {
-        throw error;
-    }
+    return await apiRequest(`/api/v2.0/robots`, "POST", robot);
 }
 
 export async function UpdateRobot(robot: Robot): Promise<Robot> {
-    try {
-        const response = await fetch(`/api/v2.0/robots`, {
-            method: "PUT",
-            headers: {'Authorization': 'Basic ' + btoa(username+':'+password)},
-            credentials: "omit",
-            body: JSON.stringify(robot)
-        });
-        if (response.ok) {
-            const robot: Robot = await response.json()
-            return robot
-        } else {
-            throw new Error("invalid response from server");
-        }
-    } catch (error) {
-        throw error;
-    }
+    return await apiRequest(`/api/v2.0/robots`, "PUT", robot);
 }
 
 export async function DeleteRobot(robotID: number): Promise<boolean> {
-    try {
-        const response = await fetch(`/api/v2.0/robots`, {
-            method: "DELETE",
-            headers: {'Authorization': 'Basic ' + btoa(username+':'+password)},
-            credentials: "omit",
-            body: JSON.stringify(robotID)
-        });
-        if (response.ok) {
-            return true
-        } else {
-            return false
-        }
-    } catch (error) {
-        throw error;
-    }
+    return await apiRequest(`/api/v2.0/robots`, "DELETE", robotID);
 }
