74898619d667417c5d0b2ebeb6a14b65871c6ba4
clayton <hello@claytonc.dev>
2025-01-03 17:27:00 -0800

    add redirects for login state

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,53 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../pages/LoginPage.vue'
+import RobotAccountsPage from '../pages/RobotAccountsPage.vue'
+import { getSavedCredentials } from '@/utils/requests'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: '/',
-      redirect: '/login'
+      redirect: '/login',
+      meta: {
+        loginPage: true
+      }
     },
     {
       path: '/login',
       name: 'login',
       component: LoginPage,
+      meta: {
+        loginPage: true
+      }
     },
     {
       path: '/robots',
       name: 'robots',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../pages/RobotAccountsPage.vue'),
+      component: RobotAccountsPage,
+      meta: {
+        requiresAuth: true
+      }
     },
   ],
 })
+
+router.beforeEach((to, _, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    try {
+      getSavedCredentials(); // Check if credentials are stored
+      next();  // If credentials are valid, allow the navigation
+    } catch (error) {
+      next('/');  // If credentials are not valid, redirect to login page
+    }
+  } else if (to.matched.some(record => record.meta.loginPage)) {
+    try {
+      getSavedCredentials()
+      next('/robots');
+    } catch (error) {
+      next()
+    }
+  }
+});
 
 export default router
