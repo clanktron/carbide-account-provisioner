169713688eef4b2ff8dc54df2dbd92f5e6384d81
clayton <hello@claytonc.dev>
2025-01-06 17:13:30 -0800

    add delete and edit followup modals

--- src/components/editRobotAccount.vue
+++ src/components/editRobotAccount.vue
@@ -20,7 +20,7 @@
           ></button>
         </div>
         <div class="modal-body">
-          <form @submit.prevent="submitForm">
+          <form @submit.prevent="submitUpdateForm">
             <div class="mb-3">
               <label for="accountDescription" class="form-label">Edit Description</label>
               <input
@@ -54,7 +54,34 @@
           </form>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-primary" @click="submitForm">Save changes</button>
+          <button type="button" class="btn btn-danger" @click="deleteAccount" data-bs-target="#deletedModal" data-bs-toggle="modal">Delete</button>
+          <button type="button" class="btn btn-primary" @click="submitUpdateForm" data-bs-target="#updatedModal" data-bs-toggle="modal">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="updatedModal" tabindex="-1" aria-labelledby="updatedModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="updatedModalLabel">Updated {{robot.name}} successfully!</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Updated robot info here
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="deletedModal" tabindex="-1" aria-labelledby="deletedModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="deletedModalLabel">Deleted {{robot.name}} successfully!</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          make this nicer
         </div>
       </div>
     </div>
@@ -62,7 +89,7 @@
 </template>
 
 <script setup lang="ts">
-import { UpdateRobot } from '@/utils/requests';
+import { DeleteRobot, UpdateRobot } from '@/utils/requests';
 import type { Robot } from '../../env';
 import { computed, ref } from 'vue';
 
@@ -85,9 +112,21 @@ const formattedExpirationDate = computed({
   },
 });
 
-async function submitForm() {
+async function submitUpdateForm() {
   try {
     const robot = await UpdateRobot(props.robot)
+  } catch (error) {
+    errorMessage.value = "An error occurred while processing your request. Please try again later.";
+  }
+}
+
+async function deleteAccount() {
+  try {
+    if (!props.robot.id) {
+      errorMessage.value = "An error occurred while processing your request. Please try again later.";
+      return
+    }
+    const result = await DeleteRobot(props.robot.id)
   } catch (error) {
     errorMessage.value = "An error occurred while processing your request. Please try again later.";
   }
