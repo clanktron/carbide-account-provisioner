108fafa66447c4093a96bee86013e3170d23498a
clayton <hello@claytonc.dev>
2025-01-13 15:21:35 -0800

    add logout dialog footer

--- src/pages/RobotAccountsPage.vue
+++ src/pages/RobotAccountsPage.vue
@@ -1,6 +1,7 @@
 <template>
   <h1 class="text-center fw-bold mb-3">Robot Accounts</h1>
   <robotAccountTable/>
+  <footer class="mt-3 text-center">To logout simply close this tab, credentials are not persisted.</footer>
 </template>
 
 <script setup lang="ts">
